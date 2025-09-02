3a2ab1a49e4c14ba6e46a09acd5fd5d9f62a3d05
AndresPescador <94087468+AndresPescador@users.noreply.github.com>
2025-09-01 22:33:24 -0500

    Update AudioController.java

--- src/main/java/com/ud/metricssoundscalculator/controller/AudioController.java
+++ src/main/java/com/ud/metricssoundscalculator/controller/AudioController.java
@@ -14,6 +14,7 @@ import java.io.File;
 
 @RestController
 @RequestMapping("/audio")
+@CrossOrigin(origins = "*")
 public class AudioController {
 
     private final AcousticService acousticService;
