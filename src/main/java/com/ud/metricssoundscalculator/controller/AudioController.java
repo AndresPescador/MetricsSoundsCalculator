2af72a698cfd44d27b2069882cf9f60b8aef80be
AndresPescador <94087468+AndresPescador@users.noreply.github.com>
2025-09-01 22:18:53 -0500

    Update AudioController.java

--- src/main/java/com/ud/metricssoundscalculator/controller/AudioController.java
+++ src/main/java/com/ud/metricssoundscalculator/controller/AudioController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
